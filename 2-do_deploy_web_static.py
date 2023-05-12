97d5fb616d8f07ade18056d632ac10462b0b408e
Ishado Emmanuel <peter.ishado@gmail.com>
2023-05-12 16:07:23 -0700

    fabric script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,50 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
-using the function do_deploy
+Fabric script (based on the file 1-pack_web_static.py) that
+       distributes an archive to your web servers
+Returns False if the file at the path archive_path doesn't exist
 """
-from fabric.api import env, run, put, hosts
-# from datetime import datetime
-import os
-
-env.hosts = ['54.175.59.46', '35.153.79.133']
+import os.path
+from fabric.api import *
+from fabric.operations import run, put, sudo
+env.hosts = ["54.175.59.46", "35.153.79.133"]
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa/'
 
-
 def do_deploy(archive_path):
+    """ script that distributes archive to web servers
+    All remote commands must be executed on your both web servers
+    (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+    Returns True if all operations has been done correctly,
+            otherwise returns False
     """
-    upload the archive to the /tmp/ directory of the web server
-    uncompress the archive
-    delete the archive from the web server
-    handle symbolic links
-    return True is all operations went well, False otherwise
-    """
-    if not os.path.exists(archive_path):
+    if (os.path.isfile(archive_path) is False):
         return False
+
     try:
-        put(archive_path, '/tmp/')
-        file_name = archive_path.split('/')[-1]
-        file_name_noext = file_name.split('.')[0]
-        new_folder = '/data/web_static/releases/' + file_name_noext + '/'
-        run('sudo mkdir -p {}'.format(new_folder))
-        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, new_folder))
-        run('sudo rm /tmp/{}'.format(file_name))
-        run('sudo mv {}web_static/* {}'.format(new_folder, new_folder))
-        run('sudo rm -rf {}web_static'.format(new_folder))
+        """Upload the archive to the /tmp/ directory of the web server"""
+        put(archive_path, "/tmp/")
+        unpack = archive_path.split("/")[-1]
+        folder = ("/data/web_static/releases/" + unpack.split(".")[0])
+        run("sudo mkdir -p {:s}".format(folder))
+
+        """Uncompress the archive to the folder
+        /data/web_static/releases/<archive filename without extension>
+        on the web server"""
+        run("sudo tar -xzf /tmp/{:s} -C {:s}".format(unpack, folder))
+
+        """Delete the archive from the web server"""
+        run("sudo rm /tmp/{:s}".format(unpack))
+        run("sudo mv {:s}/web_static/* {:s}/".format(folder, folder))
+        run("sudo rm -rf {:s}/web_static".format(folder))
+
+        """Delete the symbolic link /data/web_static/current"""
         run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(new_folder))
-        print("New version deployed!")
+
+        """Create a new the symbolic link
+           /data/web_static/current on the web server, linked to the new
+           version of your code
+           (/data/web_static/releases/<archive filename without extension>)"""
+        run("sudo ln -s {:s} /data/web_static/current".format(folder))
         return True
     except:
-        return False
-
