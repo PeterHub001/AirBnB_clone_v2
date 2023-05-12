206eff551e65b12080ac7ade5ad76386ad40a14b
Ishado Emmanuel <peter.ishado@gmail.com>
2023-05-12 16:54:52 -0700

    checking

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,49 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 1-pack_web_static.py) that
-       distributes an archive to your web servers
-Returns False if the file at the path archive_path doesn't exist
+Fabric script that generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack, and deploys it to
+your web servers using the function do_deploy
 """
-import os.path
-from fabric.api import *
-from fabric.operations import run, put, sudo
+from fabric.api import local, env, run, put
+from datetime import datetime
+import os
 env.hosts = ["54.175.59.46", "35.153.79.133"]
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+
+def do_pack():
+    """
+    create the archive file with the contents of the web_static folder
+    and return the archive path if the archive has been correctly generated
+    otherwise return None
+    """
+    datetime_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}.tgz".format(datetime_str)
+    try:
+        local('mkdir -p versions')
+        local('tar -cvzf versions/{} web_static'.format(file_name))
+        return "versions/{}".format(file_name)
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """ script that distributes archive to web servers
-    All remote commands must be executed on your both web servers
-    (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
-    Returns True if all operations has been done correctly,
-            otherwise returns False
     """
-    if (os.path.isfile(archive_path) is False):
+    distribute an archive to your web servers
+    """
+    if not os.path.exists(archive_path):
         return False
-
     try:
-        """Upload the archive to the /tmp/ directory of the web server"""
+        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
-        unpack = archive_path.split("/")[-1]
-        folder = ("/data/web_static/releases/" + unpack.split(".")[0])
-        run("sudo mkdir -p {:s}".format(folder))
-
-        """Uncompress the archive to the folder
-        /data/web_static/releases/<archive filename without extension>
-        on the web server"""
-        run("sudo tar -xzf /tmp/{:s} -C {:s}".format(unpack, folder))
-
-        """Delete the archive from the web server"""
-        run("sudo rm /tmp/{:s}".format(unpack))
-        run("sudo mv {:s}/web_static/* {:s}/".format(folder, folder))
-        run("sudo rm -rf {:s}/web_static".format(folder))
-
-        """Delete the symbolic link /data/web_static/current"""
-        run('sudo rm -rf /data/web_static/current')
-
-        """Create a new the symbolic link
-           /data/web_static/current on the web server, linked to the new
-           version of your code
-           (/data/web_static/releases/<archive filename without extension>)"""
-        run("sudo ln -s {:s} /data/web_static/current".format(folder))
+        # Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server
+        file_name = archive_path.split("/")[-1]
+        folder_name = "/data/web_static/releases/" + file_name.split(".")[0]
+        run("mkdir -p {}".format(folder_name))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_name))
+        run("rm /tmp/{}".format(file_name))
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -f /data/web_static/current")
+        # Create a new the symbolic link /data/web_static/current on the web server, linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
+        run("ln -s {} /data/web_static/current".format(folder_name))
         return True
     except:
         return False
